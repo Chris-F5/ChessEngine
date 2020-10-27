a69854dfb6ebd8f36d9e29de0b71c5313a7cc4ac
Christopher Lang <christopher-d-lang@hotmail.com>
2020-10-27 10:41:44 +0000

    Create ggez context

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use ggez::{
+    event::{self, EventHandler},
+    graphics, Context, ContextBuilder, GameResult,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .window_setup(ggez::conf::WindowSetup::default().title("Chess Engine"))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(800.0, 500.0))
+        .build()
+        .expect("error creating ggez context");
+
+    let mut my_game = ChessGame::new(&mut ctx);
+
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e),
+    }
+}
+
+struct ChessGame {}
+
+impl ChessGame {
+    pub fn new(_ctx: &mut Context) -> ChessGame {
+        ChessGame {}
+    }
+}
+
+impl EventHandler for ChessGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // TODO: recieve user input and update chess board accordingly
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        // TODO: draw chess board
+        graphics::present(ctx)
+    }
 }
